e82cc8467214c7d2be227f80f0d81b2a48225324
retr080s <retr080s@protonmail.com>
2023-07-10 18:10:11 +0200

    user added on leftnavbar (recommended)

--- src/app/components/navbarLeft.tsx
+++ src/app/components/navbarLeft.tsx
@@ -1,11 +1,25 @@
+import Image from "next/image";
+import retr080sImg from "src/assets/profile/retr080s.png";
+
 export default function navbarLeft() {
   return (
     <div className="absolute ">
       <div className="m-0  w-64 h-screen  backdrop-blur-sm bg-sky-800/30   text-sky-600 gap-4 z-39 rounded-br-lg drop-shadow-xl border-b-4 border-sky-600">
-        <ul className="text-center">
-          <p className="text-white font-bold">Recommended </p>
-          <p>test</p>
-          <p>test</p>
+        <p className="text-white font-bold text-center">Recommended </p>
+        <ul role="list" className="p-6 divide-y divide-slate-200">
+          <li className="flex py-4 first:pt-0 last:pb-0">
+            <Image
+              src={retr080sImg}
+              alt="retr080s"
+              className="h-10 w-10 rounded-full"
+            />
+            <div className="ml-3 overflow-hidden">
+              <p className="text-sm font-medium text-slate-200">Retro080s</p>
+              <p className="text-sm text-slate-400 truncate">
+                Streaming: Grand Theft Auto V
+              </p>
+            </div>
+          </li>
         </ul>
       </div>
     </div>
