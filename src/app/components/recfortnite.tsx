b4c6266ab15436c8e9536bdd6e3c37fdd5434e35
retr080s <retr080s@protonmail.com>
2023-07-11 00:13:38 +0200

    images added for all categories

--- src/app/components/recfortnite.tsx
+++ src/app/components/recfortnite.tsx
@@ -1,12 +1,79 @@
+import fortnitePic from "src/assets/featured/fortnite.png";
+import Image from "next/image";
+import { BsPerson } from "react-icons/bs";
+
 export default function recfortnite() {
   return (
     <div className="mt-8 ml-6">
       <h1 className="text-white ml-64 text-3xl">Recommended Fortnite</h1>
       <div className="  ml-64  pt-12 h-(-12)  z-38 gap-10 ">
-        <div className="flex flex-column flex-wrap justify-center gap-48">
-          <img src="https://50px.co/wp-content/uploads/2018/10/50px.gif" />
-          <img src="https://50px.co/wp-content/uploads/2018/10/50px.gif" />
-          <img src="https://50px.co/wp-content/uploads/2018/10/50px.gif" />
+        <div className="flex flex-column flex-wrap justify-center gap-5">
+          <div className="text-left ">
+            <Image
+              alt="fortnite"
+              src={fortnitePic}
+              width={450}
+              height={300}
+              className="rounded-2xl border-b-4 border-sky-600 transform duration-500 hover:scale-105"
+            />
+
+            <p className="mt-1">
+              First Stream! Join me and lets have fun! #Follow
+            </p>
+            <p className="mt-4 text-slate-400">
+              <span className="bg-sky-600/30 rounded-sm">
+                Grand Theft Auto V
+              </span>
+            </p>
+            <p className="flex justify-left  ">
+              <BsPerson className="text-white  " />
+            </p>
+            <p className="">2000 Watching</p>
+          </div>
+          <div className="text-left ">
+            <Image
+              alt="fortnite"
+              src={fortnitePic}
+              width={450}
+              height={300}
+              className="rounded-2xl border-b-4 border-sky-600 transform duration-500 hover:scale-105"
+            />
+
+            <p className="mt-1">
+              First Stream! Join me and lets have fun! #Follow
+            </p>
+            <p className="mt-4 text-slate-400">
+              <span className="bg-sky-600/30 rounded-sm">
+                Grand Theft Auto V
+              </span>
+            </p>
+            <p className="flex justify-left  ">
+              <BsPerson className="text-white  " />
+            </p>
+            <p className="">2000 Watching</p>
+          </div>
+          <div className="text-left ">
+            <Image
+              alt="fortnite"
+              src={fortnitePic}
+              width={450}
+              height={300}
+              className="rounded-2xl border-b-4 border-sky-600 transform duration-500 hover:scale-105"
+            />
+
+            <p className="mt-1">
+              First Stream! Join me and lets have fun! #Follow
+            </p>
+            <p className="mt-4 text-slate-400">
+              <span className="bg-sky-600/30 rounded-sm">
+                Grand Theft Auto V
+              </span>
+            </p>
+            <p className="flex justify-left  ">
+              <BsPerson className="text-white  " />
+            </p>
+            <p className="">2000 Watching</p>
+          </div>
         </div>
       </div>
     </div>
--- src/app/components/recgames.tsx
+++ src/app/components/recgames.tsx
@@ -1,12 +1,127 @@
+import gtaPic from "src/assets/featured/gta.png";
+import fortnitePic from "src/assets/featured/fortnite.png";
+import valorantPic from "src/assets/featured/valorant.png";
+import { BsPerson } from "react-icons/bs";
+import Image from "next/image";
+
 export default function recgames() {
   return (
     <div className="mt-8 ml-6">
-      <h1 className="text-white ml-64 text-3xl">Recommended Games</h1>
+      <h1 className="text-white  ml-64 text-3xl">Recommended Games</h1>
       <div className="  ml-64  pt-12 h-(-12)  z-38 gap-10 ">
-        <div className="flex flex-column flex-wrap justify-center gap-48">
-          <img src="https://50px.co/wp-content/uploads/2018/10/50px.gif" />
-          <img src="https://50px.co/wp-content/uploads/2018/10/50px.gif" />
-          <img src="https://50px.co/wp-content/uploads/2018/10/50px.gif" />
+        <div className="flex flex-column flex-wrap justify-center gap-5">
+          <div className="text-left ">
+            <Image
+              alt="gta"
+              src={gtaPic}
+              width={450}
+              height={300}
+              className="rounded-2xl border-b-4 border-sky-600 transform duration-500 hover:scale-105"
+            />
+
+            <p className="mt-1">
+              First Stream! Join me and lets have fun! #Follow
+            </p>
+            <p className="mt-4 text-slate-400">
+              <span className="bg-sky-600/30 rounded-sm">
+                Grand Theft Auto V
+              </span>
+            </p>
+            <p className="flex justify-left  ">
+              <BsPerson className="text-white  " />
+            </p>
+            <p className="">2000 Watching</p>
+          </div>
+          <div className="text-left">
+            <Image
+              alt="fortnite"
+              src={fortnitePic}
+              width={450}
+              height={300}
+              className="rounded-2xl border-b-4 border-sky-600 transform duration-500 hover:scale-105"
+            />
+
+            <p className="mt-1">Solo, going for 10 wins in a row #Giveaway</p>
+            <p className="mt-4 text-slate-400">
+              <span className="bg-sky-600/30 rounded-sm">Fortnite</span>
+            </p>
+            <p className="flex justify-left  ">
+              <BsPerson className="text-white  " />
+            </p>
+            <p className="">2000 Watching</p>
+          </div>
+          <div className="text-left">
+            <Image
+              alt="valorant"
+              src={valorantPic}
+              width={450}
+              height={300}
+              className="rounded-2xl border-b-4 border-sky-600 transform duration-500 hover:scale-105"
+            />
+
+            <p className="mt-1">Playing ranked #tryhard #win</p>
+            <p className="mt-4 text-slate-400">
+              <span className="bg-sky-600/30 rounded-sm">Valorant</span>
+            </p>
+            <p className="flex justify-left  ">
+              <BsPerson className="text-white  " />
+            </p>
+            <p className="">2000 Watching</p>
+          </div>
+          <div className="text-left">
+            <Image
+              alt="valorant"
+              src={valorantPic}
+              width={450}
+              height={300}
+              className="rounded-2xl border-b-4 border-sky-600 transform duration-500 hover:scale-105"
+            />
+
+            <p className="mt-1">Playing ranked #tryhard #win</p>
+            <p className="mt-4 text-slate-400">
+              <span className="bg-sky-600/30 rounded-sm">Valorant</span>
+            </p>
+            <p className="flex justify-left  ">
+              <BsPerson className="text-white  " />
+            </p>
+            <p className="">2000 Watching</p>
+          </div>
+          <div className="text-left">
+            <Image
+              alt="valorant"
+              src={valorantPic}
+              width={450}
+              height={300}
+              className="rounded-2xl border-b-4 border-sky-600 transform duration-500 hover:scale-105"
+            />
+
+            <p className="mt-1">Playing ranked #tryhard #win</p>
+            <p className="mt-4 text-slate-400">
+              <span className="bg-sky-600/30 rounded-sm">Valorant</span>
+            </p>
+            <p className="flex justify-left  ">
+              <BsPerson className="text-white  " />
+            </p>
+            <p className="">2000 Watching</p>
+          </div>
+          <div className="text-left">
+            <Image
+              alt="valorant"
+              src={valorantPic}
+              width={450}
+              height={300}
+              className="rounded-2xl border-b-4 border-sky-600 transform duration-500 hover:scale-105"
+            />
+
+            <p className="mt-1">Playing ranked #tryhard #win</p>
+            <p className="mt-4 text-slate-400">
+              <span className="bg-sky-600/30 rounded-sm">Valorant</span>
+            </p>
+            <p className="flex justify-left  ">
+              <BsPerson className="text-white  " />
+            </p>
+            <p className="">2000 Watching</p>
+          </div>
         </div>
       </div>
     </div>
