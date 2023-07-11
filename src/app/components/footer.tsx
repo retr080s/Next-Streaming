f3ea99bf8be4da6fb5a03f8c7f0cbe7dae2951f1
retr080s <retr080s@protonmail.com>
2023-07-12 00:58:22 +0200

    added pages for all footer links

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -9,22 +9,36 @@ export default function footer() {
           Streaming App
         </span>
       </h1>
-      <div className="flex flex-row justify-center  mt-16 h-screen gap-4 m-0 bg-black/30 ">
+      <div className="flex flex-row justify-center  mt-16 h-screen gap-4 m-0 bg-black/30  ">
         <p>Policy:</p>
         <ul>
           <Link href="/guidelines">
-            <li>-Guidelines</li>
+            <li className="hover:text-slate-400">-Guidelines</li>
           </Link>
-          <li>-Privacy Policy</li>
-          <li>-Terms of service</li>
-          <li>-Help</li>
+          <Link href="/privacypolicy">
+            <li className="hover:text-slate-400">-Privacy Policy</li>
+          </Link>
+          <Link href="/tos">
+            <li className="hover:text-slate-400">-Terms of service</li>
+          </Link>
+          <Link href="help">
+            <li className="hover:text-slate-400">-Help</li>
+          </Link>
         </ul>
         <p>Resources:</p>
         <ul>
-          <li>-Support</li>
-          <li>-Contact</li>
-          <li>-Careers</li>
-          <li>-Partner Program</li>
+          <Link href="/support">
+            <li className="hover:text-slate-400">-Support</li>
+          </Link>
+          <Link href="/contact">
+            <li className="hover:text-slate-400">-Contact</li>
+          </Link>
+          <Link href="/careers">
+            <li className="hover:text-slate-400">-Careers</li>
+          </Link>
+          <Link href="/partnerprogram">
+            <li className="hover:text-slate-400">-Partner Program</li>
+          </Link>
         </ul>
       </div>
     </div>
---
+++ src/app/tos/page.tsx
@@ -0,0 +1,24 @@
+import NavbarTop from "../components/NavbarTop";
+export default function page() {
+  return (
+    <div>
+      <NavbarTop />
+      <div className="flex flex-col text-center justify-center  items-center mt-28 ">
+        <h1 className="text-3xl mb-2">Terms of service</h1>
+        <p className=" bg-sky-800/30 drop-shadow-xl backdrop-blur-sm w-80 rounded-md">
+          Pie gummi bears dragée cheesecake gummies candy. Macaroon soufflé
+          pudding halvah shortbread. Brownie ice cream croissant danish
+          fruitcake lemon drops dragée. Jelly marshmallow jelly beans donut
+          toffee biscuit. Sweet marshmallow sesame snaps sweet roll toffee
+          carrot cake. Gingerbread gingerbread pastry soufflé chocolate bar.
+          Danish fruitcake jelly carrot cake tootsie roll chocolate bar jelly-o.
+          Brownie cake danish cookie sesame snaps. Gummi bears marshmallow
+          danish gingerbread toffee shortbread cupcake. Candy sugar plum carrot
+          cake oat cake tart powder fruitcake. Ice cream bonbon lollipop candy
+          muffin cake marshmallow bear claw. Cake jelly-o tootsie roll sweet
+          caramels tart.
+        </p>
+      </div>
+    </div>
+  );
+}
