3795974dd0734a9f153c0119871978fd63b1a5b8
retr080s <retr080s@protonmail.com>
2023-07-14 23:34:28 +0200

    autoplay added to streaming page

--- src/app/streaming/page.tsx
+++ src/app/streaming/page.tsx
@@ -18,10 +18,11 @@ export default function page() {
       <NavbarTop />
       <div className="flex justify-center mt-14">
         <MediaPlayer
+          autoplay
           style={{ width: 1400, height: 788 }}
           title="🔴LIVE"
-          src="https://cdn.pixabay.com/vimeo/580974597/spacecraft-82659.mp4?rendition=source&expiry=1689356937&hash=6913186ed4c2892e53af5b2e68512324af19eee6"
-          poster="https://cdn.pixabay.com/vimeo/580974597/spacecraft-82659.mp4?rendition=source&expiry=1689356937&hash=6913186ed4c2892e53af5b2e68512324af19eee6"
+          src="https://vod-progressive.akamaized.net/exp=1689383360~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2435%2F19%2F487176852%2F2183603136.mp4~hmac=53d8fb981e7fd49b24516a64cc05cf7723475b7ec1d3f5ae28765132e657594f/vimeo-prod-skyfire-std-us/01/2435/19/487176852/2183603136.mp4?filename=file.mp4"
+          poster="https://vod-progressive.akamaized.net/exp=1689383360~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2435%2F19%2F487176852%2F2183603136.mp4~hmac=53d8fb981e7fd49b24516a64cc05cf7723475b7ec1d3f5ae28765132e657594f/vimeo-prod-skyfire-std-us/01/2435/19/487176852/2183603136.mp4?filename=file.mp4"
           // thumbnails="https://media-files.vidstack.io/sprite-fight/thumbnails.vtt"
           aspectRatio={16 / 9}
           crossorigin=""
